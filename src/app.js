57482909bdb4962d1bc3cc0a67f9c9bcaff86e3d
Ravi Gupta <ravirocks325@gmail.com>
2018-11-28 14:36:12 +0530

    Created a pure React component

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed)
+    ]);
+}
+
+class App extends React.Component {
+    handleTitleClick() {
+        alert("you clicked the title");
+    }
+
+    render() {
+        return React.createElement("div", {}, [
+            React.createElement("h1", { onClick: this.handleTitleClick }, 'Adopt Me!'),
+            React.createElement(Pet, {
+                name: "Luna",
+                animal: "Dog",
+                breed: "Havanese",
+            }),
+            React.createElement(Pet, {
+                name: "Pepper",
+                animal: "Bird",
+                breed: "Cockatiel",
+            }),
+            React.createElement(Pet, {
+                name: "Donik",
+                animal: "Dog",
+                breed: "Mixed",
+            })
+        ]);
+    }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));1
